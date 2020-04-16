38414deeb91cad910d5d70196eb201905a72231a
Oscar <oscar.ramos@ucsp.edu.pe>
2020-04-15 19:20:37 -0500

    Updates

---
+++ precache-manifest.6963e57f3e94b75c0776e25b887b5d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4207dff0c0cfb1c48603d29bc3231b75",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "acd74f122528a1913271",
+    "url": "/quiz-app/static/css/main.bfe787c4.chunk.css"
+  },
+  {
+    "revision": "a9a41324289e7dec8df0",
+    "url": "/quiz-app/static/js/2.be4f2ff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.be4f2ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd74f122528a1913271",
+    "url": "/quiz-app/static/js/main.87d777d0.chunk.js"
+  },
+  {
+    "revision": "c9b5d4e05514bb91e020",
+    "url": "/quiz-app/static/js/runtime-main.b8fe501a.js"
+  }
+]);
